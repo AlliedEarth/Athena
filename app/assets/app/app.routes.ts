18ce266d1d972c53329b9f8fdc79f3a01708031f
zcourts <courtney@crlog.info>
2016-06-22 21:52:20 +0100

    messing with angular2#master's tour of heros tutorial

--- app/assets/app/app.routes.ts
+++ app/assets/app/app.routes.ts
@@ -1,9 +1,11 @@
-import {provideRouter, RouterConfig} from "@angular/router"
+import { provideRouter, RouterConfig } from '@angular/router';
 
-import {TodoAppComponent}  from "./app"
+export const routes: RouterConfig = [
+    // { path: 'crisis-center', component: CrisisCenterComponent },
+    // { path: 'heroes', component: HeroListComponent },
+    // { path: 'hero/:id', component: HeroDetailComponent }
+];
 
-export const routes:RouterConfig = [
-    {path: "/", component: TodoAppComponent},
-]
-
-export const APP_ROUTER_PROVIDERS = [provideRouter(routes)]
+export const APP_ROUTER_PROVIDERS = [
+    provideRouter(routes)
+];
