595a75fd645f140ae247c28db855ea362311d92e
zcourts <courtney@crlog.info>
2016-07-16 05:47:23 +0100

    attempt a gradle setup

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-//http://stackoverflow.com/a/35514492/400048
-// Typings needed for compilation with --target=es5
-///<reference path="../target/web/node-modules/main/webjars/angular2/typings/browser.d.ts"/>
-
-///<reference path="browser/ambient/jasmine/index.d.ts" />
